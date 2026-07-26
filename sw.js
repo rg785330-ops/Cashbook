c097848b354ca67b4b59dffc4c3afacaad2e12a8
rg785330-ops <rg785330@gmail.com>
2026-07-26 21:16:33 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+/* Cashbook offline cache */
+var CACHE = "cashbook-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-maskable.png"
+];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){
+      return Promise.all(ASSETS.map(function(u){
+        return c.add(u).catch(function(){});
+      }));
+    })
+  );
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){
+        return k === CACHE ? null : caches.delete(k);
+      }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function(e){
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      if(hit) return hit;
+      return fetch(e.request).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, copy).catch(function(){}); });
+        return res;
+      }).catch(function(){
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
